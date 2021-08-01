f0d5906658d7d55a01d9720f52d377b2f5040eb0
Samuel Quiñones <sdquinones1@gmail.com>
2021-08-01 12:07:38 -0400

    added bootstrap container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
-const colors = require("tailwindcss/colors");
-
-delete colors.lightBlue;
-
 module.exports = {
   purge: ["./src/pages/**/*.tsx", "./src/components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    container: {
-      padding: {
-        DEFAULT: "0.5rem",
-        md: "1rem",
-        lg: "6rem",
-        xl: "7rem",
-        "2xl": "8rem",
-      },
-    },
-  },
+  theme: {},
   variants: {
     extend: {},
   },
