87c1a696c46dba419af1d23622855a1aa792cb3f
Samuel Quiñones <sdquinones1@gmail.com>
2022-03-06 19:01:47 -0500

    added new icon

--- src/util/FontAwesome.ts
+++ src/util/FontAwesome.ts
@@ -6,9 +6,19 @@ import {
   faTwitter,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
-import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleUp as faAngleUpSolid,
+  faClose as faCloseSolid,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const initializeFontAwesome = () => {
   config.autoAddCss = false;
-  library.add(faDiscord, faGithub, faTwitter, faYoutube, faAngleUp);
+  library.add(
+    faDiscord,
+    faGithub,
+    faTwitter,
+    faYoutube,
+    faAngleUpSolid,
+    faCloseSolid
+  );
 };
