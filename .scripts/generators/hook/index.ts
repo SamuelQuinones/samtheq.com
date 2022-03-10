a54d645b08c17601ece70fada0ac61373f593492
Samuel Quiñones <sdquinones1@gmail.com>
2022-03-10 14:28:52 -0500

    added custom hook generator

---
+++ .scripts/generators/hook/index.ts
@@ -0,0 +1,50 @@
+import { Actions, PlopGeneratorConfig } from "node-plop";
+import * as path from "path";
+
+const hooksDirectory = path.join(process.cwd(), "src/hooks");
+const templateDir = path.join(process.cwd(), ".scripts/generators/hook");
+
+export enum HookPromptConfig {
+  hookName = "hookName",
+}
+
+type Answers = { [P in HookPromptConfig]: string };
+
+export const HookGenerator: PlopGeneratorConfig = {
+  description: "Generate custom hooks",
+  prompts: [
+    {
+      type: "input",
+      name: HookPromptConfig.hookName,
+      message: "What is the name of your hook?",
+    },
+  ],
+  actions: (data) => {
+    const answers = data as Answers;
+    const actions: Actions = [];
+
+    const newHookPath = `${hooksDirectory}/{{kebabCase ${HookPromptConfig.hookName}}}.ts`;
+    const allHooksPath = `${hooksDirectory}/index.ts`;
+
+    actions.push({
+      type: "add",
+      path: newHookPath,
+      templateFile: `${templateDir}/template.hbs`,
+      abortOnFail: true,
+    });
+    actions.push({
+      type: "append",
+      path: allHooksPath,
+      pattern: /(\/\/ APPEND HERE)/g,
+      template: `export { default as {{camelCase ${HookPromptConfig.hookName}}} } from "./{{kebabCase ${HookPromptConfig.hookName}}}";`,
+      abortOnFail: true,
+    });
+    actions.push({
+      type: "lintify",
+      data: {
+        path: "src/hooks",
+      },
+    });
+    return actions;
+  },
+};
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
+// APPEND HERE
 export { default as useOutsideClick } from "./use-outside-click";
 export { default as useOnRouterEvent } from "./use-router-event";
 export { default as useEventListener } from "./use-event-listener";
