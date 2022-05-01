d885693a4f17eb9203b21573abd25c38ac3b6adf
Samuel Quiñones <sdquinones1@gmail.com>
2022-05-01 19:14:07 -0400

    updated skeleton styles

--- src/components/UpdateFeed/Skeleton.tsx
+++ src/components/UpdateFeed/Skeleton.tsx
@@ -5,25 +5,25 @@ const UpdateItemSkeleton = () => {
   return (
     <Card className="bg-gray-900 text-white">
       <div className="flex animate-pulse flex-col justify-between">
-        <p>
-          <span className="text-placeholder mb-1 w-[58%]" />{" "}
-          <span className="text-placeholder mb-1 w-[33%]" />{" "}
-          <span className="text-placeholder mb-1 w-[33%]" />{" "}
-          <span className="text-placeholder mb-1 w-[50%]" />{" "}
-          <span className="text-placeholder mb-1 w-[66%]" />
+        <p className="mb-1">
+          <span className="text-placeholder w-7/12" />{" "}
+          <span className="text-placeholder w-1/3" />{" "}
+          <span className="text-placeholder w-1/3" />{" "}
+          <span className="text-placeholder w-1/2" />{" "}
+          <span className="text-placeholder w-2/3" />
         </p>
         <div className="-m-4 mt-4 flex justify-end rounded-b-md py-2 px-4">
           <Button
             href="#"
             tabIndex={-1}
             disabled
-            className="text-placeholder w-[40%] before:inline-block before:content-['']"
+            className="text-placeholder w-1/3 before:inline-block before:content-['']"
             aria-hidden="true"
           />
         </div>
-        <p className="-m-2 mt-4">
-          <span className="text-placeholder mb-1 w-[20%]" />{" "}
-          <span className="text-placeholder mb-1 w-[20%]" />
+        <p className="-m-2 mt-4 mb-1">
+          <span className="text-placeholder w-1/5" />{" "}
+          <span className="text-placeholder w-1/5" />
         </p>
       </div>
     </Card>
