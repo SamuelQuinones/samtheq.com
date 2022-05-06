266adb35b6bfa2b32d1c91343a71c35f5c97ab6e
Samuel Quiñones <sdquinones1@gmail.com>
2022-05-05 23:27:46 -0400

    added redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/about",
+        destination: "/",
+        permanent: false,
+      },
+      {
+        source: "/posts/:slug*",
+        destination: "/blog/:slug*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
