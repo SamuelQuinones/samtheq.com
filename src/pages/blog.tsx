80e2c304ae4bb4c5674bb233eb2842bfa24a1f8c
Samuel Quiñones <sdquinones1@gmail.com>
2023-02-08 23:48:34 -0500

    minor changes to appearance

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,10 +4,15 @@ import PageLayout from "layout/Page";
 const Blog: NextPage = () => {
   return (
     <PageLayout title="Blog" pageUrl="/blog">
-      <h1 className="mb-8 text-center text-4xl sm:text-5xl lg:text-6xl">
-        Blog
+      <h1 className="mb-8 text-center text-4xl">
+        A Separate Blog site is on the way!
       </h1>
-      <p>I am moving the blog to its own site</p>
+      <p className="md:text-l lg:text-xl">
+        I have elected to have the blog be its own websites that this one will
+        link to. I came to this decision because I wanted to allow myself to add
+        as many features as I wanted to the blog without bloating the main
+        codebase
+      </p>
     </PageLayout>
   );
 };
