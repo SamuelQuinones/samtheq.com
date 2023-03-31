70037569a6d8fb41b6fcd9183c71dc8a96e9c164
Samuel Quiñones <sdquinones1@gmail.com>
2023-03-31 09:53:52 -0400

    Updated addServerIcons notes

--- src/lib/FontAwesome/server.ts
+++ src/lib/FontAwesome/server.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { faGithub, faLinkedinIn, faTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
-const { library } = require("@fortawesome/fontawesome-svg-core");
+const { library, config } = require("@fortawesome/fontawesome-svg-core");
 
 /**
- * MArch 30th 2023
+ * April 1st 2023
  * Because NextJS wants to not load icons cross-env, we have to load some icons on the server and some on the client ... love that
  *
- * LOOKS LIKE AUTOADDCSS MIGHT NOT BE NEEDED ON SERVER. BE ON THE LOOKOUT DOR DUPLICATE STYLES IN A STYLE TAG
+ * LOOKS LIKE AUTOADDCSS MIGHT NOT BE NEEDED ON SERVER.
  */
 export default function addServerIcons() {
+  config.autoAddCss = false;
   library.add(faYoutube, faGithub, faLinkedinIn, faTwitter);
 }
