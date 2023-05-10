f640d2b96a38b925a7d5d3fabe18bb70f4fb61b7
Samuel Quiñones <sdquinones1@gmail.com>
2023-05-09 23:08:51 -0400

    removed unused hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,5 @@
 // APPEND HERE
 export { default as useMergedRef } from "./use-merged-ref";
-export { default as useWindowSize } from "./use-window-size";
 export { default as useOutsideClick } from "./use-outside-click";
 export { default as useEventListener } from "./use-event-listener";
 export { default as useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect";
