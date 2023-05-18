e8eea7b01387df04c74f9360c4e67620935395a6
Samuel Quiñones <sdquinones1@gmail.com>
2023-05-18 00:02:19 -0400

    made experience loading nicer

--- src/app/experience/loading.tsx
+++ src/app/experience/loading.tsx
@@ -1,3 +1,5 @@
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import ExperienceFooter from "./Footer";
 import ExperienceHeader from "./Header";
 
@@ -5,7 +7,11 @@ export default function ExperienceLoading() {
   return (
     <main id="stq-page-content" className="bs-container-md mt-16 flex grow scroll-mt-16 flex-col">
       <ExperienceHeader lastUpdated="Loading..." />
-      <div className="flex grow items-center justify-center">Loading...</div>
+      <div className="mt-8 flex grow flex-col items-center justify-center gap-10">
+        <h3 className="text-lg md:text-2xl">Loading Timeline items</h3>
+        <FontAwesomeIcon height="1em" size="8x" icon={faRotate} spin />
+        <h3 className="text-lg md:text-2xl">Please wait</h3>
+      </div>
       <ExperienceFooter />
     </main>
   );
