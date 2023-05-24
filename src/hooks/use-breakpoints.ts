7b8672577a6cb75cc0e8d36e7b982ba0957b048f
Samuel Quiñones <sdquinones1@gmail.com>
2023-05-23 23:25:42 -0400

    updated use-breakpoints

--- src/hooks/use-breakpoints.ts
+++ src/hooks/use-breakpoints.ts
@@ -1,11 +1,11 @@
 import useMediaQuery from "./use-media-query";
 
 export default function useBreakpoints() {
-  const isSmall = useMediaQuery("not all and (min-width: 640px)");
-  const isMedium = useMediaQuery("not all and (min-width: 768px)");
-  const isLarge = useMediaQuery("not all and (min-width: 1024px)");
-  const isExtraLarge = useMediaQuery("not all and (min-width: 1280px)");
-  const isDoubleExtraLarge = useMediaQuery("not all and (min-width: 1536px)");
+  const isSmall = useMediaQuery("(min-width: 640px) and (max-width: 767px)");
+  const isMedium = useMediaQuery("(min-width: 768px) and (max-width: 1023px)");
+  const isLarge = useMediaQuery("(min-width: 1024px) and (max-width: 1279px)");
+  const isExtraLarge = useMediaQuery("(min-width: 1280px) and (max-width: 1535px)");
+  const isDoubleExtraLarge = useMediaQuery("(min-width: 1536px)");
   const isExtraSmall = !isSmall && !isMedium && !isLarge && !isExtraLarge && !isDoubleExtraLarge;
 
   return { isExtraSmall, isSmall, isMedium, isLarge, isExtraLarge, isDoubleExtraLarge };
