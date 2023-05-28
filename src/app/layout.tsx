f00e24af48f5a43d77822fa4c6d1730551db1465
Samuel Quiñones <sdquinones1@gmail.com>
2023-05-28 00:14:46 -0400

    prevent autoAddCsss on server

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Template from "./_client";
 import { mergeMetadata } from "@/lib/NextJS/metadata";
-// import { config } from "@fortawesome/fontawesome-svg-core";
+import { config } from "@fortawesome/fontawesome-svg-core";
 
-// config.autoAddCss = false;
+config.autoAddCss = false;
 
 const InterVar = localFont({
   src: [
