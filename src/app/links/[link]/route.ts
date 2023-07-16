078d6bce4c65acfe7b9f2c9934d35e741f7f45c0
Samuel Quiñones <sdquinones1@gmail.com>
2023-07-16 12:17:50 -0400

    fixed trycatch issue

--- src/app/links/[link]/route.ts
+++ src/app/links/[link]/route.ts
@@ -5,18 +5,24 @@ interface Context {
   params: { link?: string };
 }
 
+/**
+ * July 16th 2023
+ * This does not work with a normal try catch because redirect throws an error.
+ * https://github.com/vercel/next.js/blob/canary/packages/next/src/client/components/redirect.ts#L16-L41C2
+ */
 export async function GET(req: Request, { params }: Context) {
   const link = params.link;
   // eslint-disable-next-line eqeqeq
   if (link == undefined) return notFound();
 
-  try {
-    const SOCIAL_LINK = await prisma.socialLink.findFirstOrThrow({
+  const SOCIAL_LINK = await prisma.socialLink
+    .findFirstOrThrow({
       where: { redirect: link, active: true },
       select: { target: true },
-    });
-    redirect(SOCIAL_LINK.target);
-  } catch (error) {
+    })
+    .catch(() => null);
+  if (SOCIAL_LINK === null) {
     redirect("/links");
   }
+  redirect(SOCIAL_LINK.target);
 }
