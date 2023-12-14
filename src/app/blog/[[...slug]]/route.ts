dbfd86b9abe8f3ffaa5b9b359548b826b4d4a105
Samuel Quiñones <sdquinones1@gmail.com>
2023-12-13 23:22:32 -0500

    oops

--- src/app/blog/[[...slug]]/route.ts
+++ src/app/blog/[[...slug]]/route.ts
@@ -7,7 +7,7 @@ interface Context {
 export function GET(req: Request, { params }: Context) {
   const slug = (params.slug || []).join("/");
 
-  return NextResponse.redirect(new URL(`/${slug.slice(1)}`, process.env.NEXT_PUBLIC_BLOG_URL), {
+  return NextResponse.redirect(new URL(`/${slug}`, process.env.NEXT_PUBLIC_BLOG_URL), {
     status: 308,
   });
 }
